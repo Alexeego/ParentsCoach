c1ec9f8b55d716ef28465c789d25f4d5504fcf1c
Alexeego <Alexey.v13@hotmail.com>
2016-10-16 05:21:00 +0200

    Commit

--- app/src/main/java/com/example/alexey/parentscoach/MainWindowFragment.java
+++ app/src/main/java/com/example/alexey/parentscoach/MainWindowFragment.java
@@ -4,7 +4,6 @@ package com.example.alexey.parentscoach;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
